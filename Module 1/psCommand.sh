04389735b4d7b089017c64eebc3f09c04ad1b5d8
yokeshkumar7105 <yokeshkumarcse@gmail.com>
2026-01-30 16:53:13 +0530

    m1q4 added

---
+++ Module 1/psCommand.sh
@@ -0,0 +1,10 @@
+# 4.use ps to display all type of process running in system
+
+#list process in current terminal
+ps
+
+#show all process shows with pid, tty, time, cmd
+ps -e
+
+# details of all process then,
+ps -ef
