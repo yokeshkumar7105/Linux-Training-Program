45b4e7921ce047d9fd2813bc6ce6f7b9ed4db75b
yokeshkumar7105 <yokeshkumarcse@gmail.com>
2026-02-04 22:13:53 +0530

    m4q1 added

---
+++ Module 4/extraction.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+if [ $# -ne 1 ]; then
+    echo "give correct arguments"
+    exit 1
+fi
+inp_file="$1"
+op_file="output.txt"
+if [ ! -f "$inp_file" ]; then
+    echo "Error: Input file does not exist."
+    exit 1
+fi
+> "$op_file"
+while IFS= read -r line; do
+    if [[ "$line" == *"frame.time"* ]]; then
+        echo "\"frame.time\": \"${line#*: }\"" >> "$op_file"
+    fi
+    if [[ "$line" == *"wlan.fc.type"* ]]; then
+        echo "\"wlan.fc.type\": \"${line#*: }\"" >> "$op_file"
+    fi
+    if [[ "$line" == *"wlan.fc.subtype"* ]]; then
+        echo "\"wlan.fc.subtype\": \"${line#*: }\"" >> "$op_file"
+    fi
+done < "$inp_file"
+echo "output saved as output.txt"
